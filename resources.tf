79c946ecee074ba41d86f24ca2ff3c4163a35e17
Quique <ncnr_quique@hotmail.com>
2024-09-23 10:08:36 -0500

    init terraform

---
+++ resources.tf
@@ -0,0 +1,55 @@
+resource "aws_instance" "linux" {
+  ami           = "ami-03e383d33727f4804"
+  instance_type = "t2.medium"
+
+  key_name = "aws-keypair"
+  vpc_security_group_ids = [aws_security_group.debian_sg.id]
+  tags = {
+    Name = "debian"
+    Environment = "test"
+  }
+}
+
+resource "aws_instance" "windows" {
+  ami           = "ami-0845068028e672a07"
+  instance_type = "t2.medium"
+
+  key_name = "aws-keypair"
+  vpc_security_group_ids = [aws_security_group.debian_sg.id]
+  tags = {
+    Name = "windows"
+    Environment = "qa"
+  }
+}
+
+resource "aws_security_group" "debian_sg" {
+  name = "debian-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }    
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
